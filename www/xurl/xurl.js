45dcfa346b2d63555183b022877386467714dc35
Juan Núñez <juan@handpoint.com>
2017-06-26 15:46:40 +0200

    started adding xurl

--- www/xurl/xurl.js
+++ www/xurl/xurl.js
@@ -8,7 +8,27 @@ function XurlEngine() {
      * Protocol Version enum
      * @type Object
      */
-    this.ProtocolVersions = Object.freeze({Unknown: "", V1:"v1", V2:"v2"});
+    this.protocol = function()
+    {
+        this.versions = function () {
+            this.v1 = 'v1';
+            this.v2 = 'v2';
+        }
 
+        this.protocolFromString = function (version) {
+            if (version == this.versions.v1) {
+                return V1Engine();
+            }
+            else if (version == this.versions.v2) {
+                return V2Engine();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    this
 
 }
