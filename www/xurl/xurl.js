1ad4f60bac16b0b14343a7ccb7a2f33f04171267
Juan Núñez <juan@handpoint.com>
2017-06-26 13:35:23 +0200

    Adding XURL engine

---
+++ www/xurl/xurl.js
@@ -0,0 +1,14 @@
+/**
+ * Constructor.
+ * @returns {XurlEngine}
+ */
+function XurlEngine() {
+
+    /**
+     * Protocol Version enum
+     * @type Object
+     */
+    this.ProtocolVersions = Object.freeze({Unknown: "", V1:"v1", V2:"v2"});
+
+
+}
