3a8a82c3b172b2e4c33056cdedbbd55ffb8d61ea
Matthías Ásgeirsson <matthias.asgeirsson@gmail.com>
2017-09-27 16:33:46 +0000

    Adding some code and readme text
    I will never finish this, but can put in the base for further work.

--- src/ios/HapiCordova.m
+++ src/ios/HapiCordova.m
@@ -29,4 +29,63 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+- (void)didFindAccessoryDevice:(HeftRemoteDevice*)newDevice{
+    heftClient = nil;
+    [[HeftManager sharedManager] clientForDevice:newDevice sharedSecret:[[NSData alloc] initWithBytes:ss length:sizeof(ss)] delegate:self];
+}
+
+// example from testclient, just add methods as needed
+- (void)startDiscovery:(BOOL)fDiscoverAllDevices
+{
+    [[HeftManager sharedManager] startDiscovery:fDiscoverAllDevices];
+}
+
+- (void)didConnect:(id<HeftClient>)client{
+    NSLog(@"didConnect");
+    heftClient = client;
+}
+
+- (BOOL)saleWithAmount:(NSInteger)amount currency:(NSString*)currency cardholder:(BOOL)present
+{
+
+}
+
+- (BOOL)refundWithAmount:(NSInteger)amount currency:(NSString*)currency cardholder:(BOOL)present
+{
+
+}
+
+// device management
+- (void)resetDevices
+{
+
+}
+
+- (void)clientForDevice:(HeftRemoteDevice*)device sharedSecret:(NSData*)sharedSecret delegate:(NSObject<HeftStatusReportDelegate>*)aDelegate
+{
+    NSLog(@"hpHeftService clientForDevice");
+    [[HeftManager sharedManager] clientForDevice:device sharedSecret:sharedSecret delegate:aDelegate];
+}
+
+- (BOOL)financeInit
+{
+
+}
+
+- (void) logSetLevel:(eLogLevel)level
+{
+
+}
+
+- (BOOL) logReset
+{
+
+}
+- (BOOL) logGetInfo
+{
+
+}
+
+
+
 @end
