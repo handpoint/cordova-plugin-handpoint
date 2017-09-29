376b024bf129a09578b6086fbf52a6e9e630b477
Matthías Ásgeirsson <matthias.asgeirsson@gmail.com>
2017-09-29 14:13:50 +0000

    Just notes
    For anyone trying to work on this.

--- src/ios/HapiCordova.m
+++ src/ios/HapiCordova.m
@@ -12,17 +12,35 @@
     [manager resetDevices];
 }
 
+// map method names - but this could even be a map from string to function pointer, instead of function name
+NSDictionary *MethodDictionary = @{
+    @"sale" : @"saleWithAmount",
+    @"refund" : @"refundWithAmount"
+    // ... complete list with the rest of the
+    // methods..
+};
+
 - (void)execute:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* command = [command.arguments objectAtIndex:0];
+    // NSString* command = [command.arguments objectAtIndex:0];
     NSString* method_name = command.methodName;
 
     if (method_name != nil && [method_name length] > 0) {
 
         // we must translate the names, since the methods do not have the same names as in Android/.Net
+        NSString* translated_method_name = MethodDictionary[method_name];
+        if (translated_method_name != nil)
+        {
+            // call the given method, with the parameters it needs
+            if (translated_method_name == 'saleWithAmount')
+            {
+                NSInteger amount = 
+- (BOOL)saleWithAmount:(NSInteger)amount currency:(NSString*)currency cardholder:(BOOL)present
 
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+            }
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+        }
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
@@ -86,6 +104,34 @@
 {
 
 }
+
+- (void)responseFinanceStatus:(id<FinanceResponseInfo>)info;
+{
+    NSMutableSet *saleSet = [NSMutableSet setWithObjects:@"APPROVED", @"AUTHORISED", @"DECLINED", @"CANCELLED", @"CARD BLOCKED", nil];
+    
+    NSLog(@"hpHeftService responseFinanceStatus");
+    NSLog(@"%@", info.status);
+    NSLog(@"%@", info.xml.description);
+    NSString* financialStatus = [info.xml objectForKey:@"FinancialStatus"];
+    if ([saleSet containsObject:financialStatus]) 
+    {
+        receipt = [self generateReceipt:info];
+        
+        if ([[info customerReceipt] length] > 0 || [[info merchantReceipt] length] > 0) {
+            [receiptDelegate addItem:receipt];
+        }
+        
+        webReceipt = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 595, 0)];
+        [webReceipt setDelegate:self];
+        [webReceipt loadHTMLString:receipt.merchantReceipt baseURL:nil];
+        NSLog(@"Webview is loading...");
+    }    
+
+    // and then return some data to javascripot
+    [self.commandDelegate sendPluginResult:X callbackId:command.callbackId];
+
+}
+
 
 
 
