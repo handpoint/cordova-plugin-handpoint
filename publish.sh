89081cc51d33185b60d0681f143ce52af6f20665
Juan Núñez <juan@handpoint.com>
2017-10-18 18:56:20 +0200

    Some more minor shit

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 git push origin master;
-NEWVERSION=$(git ls-remote --tags -q | tail -n 1 -r | cut -f 2 | cut -d \/ -f 3 | perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e');
+NEWVERSION=$( git ls-remote --tags -q | cut -f 2 | cut -d \/ -f 3 | sort -t. -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr | head -n 1 | perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e');
 git tag $NEWVERSION;
 git push origin --tags;
