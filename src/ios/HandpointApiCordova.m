88e5d173f5cb35c6cf98be83b063a8e7fa098af7
Ezequiel Cuñado <ecunado@gmail.com>
2017-10-25 10:43:42 +0000

    Hello world plugin

--- src/ios/HandpointApiCordova.m
+++ src/ios/HandpointApiCordova.m
@@ -2,6 +2,61 @@
 
 @implementation HandpointApiCordova
 
+- (void)pluginInitialize
+{
+
+}
+
+- (void)sale:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)saleReversal:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)refund:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)refundReversal:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)cancelRequest:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)tipAdjustment:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)signatureResult:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)connect:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)disconnect:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)setSharedSecret:(CDVInvokedUrlCommand*)command
+{
+
+}
+
 - (void)setup:(CDVInvokedUrlCommand*)command
 {
 
@@ -13,6 +68,53 @@
                                messageAsString:msg];
 
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+- (void)setParameter:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)setLogLevel:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)getDeviceLogs:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)getPendingTransaction:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)update:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)listDevices:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)startMonitoringConnections:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)stopMonitoringConnections:(CDVInvokedUrlCommand*)command
+{
+
+}
+
+- (void)eventHandler:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
