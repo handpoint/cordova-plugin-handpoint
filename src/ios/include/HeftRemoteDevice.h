0c5f3682f5851370354e7c1abb20d578b750f0ef
raul.carrasco <raul@handpoint.com>
2017-12-26 14:02:21 +0100

    Fixed SDK connection

--- src/ios/include/HeftRemoteDevice.h
+++ src/ios/include/HeftRemoteDevice.h
@@ -8,8 +8,8 @@
 
 @interface HeftRemoteDevice : NSObject
 
-@property(nonatomic, readonly) NSString* name;
-@property(nonatomic, readonly) NSString* address;
-@property(nonatomic, readonly) EAAccessory* accessory;
+- (NSString *)name;
+- (NSString *)address;
+- (EAAccessory *)accessory;
 
 @end
