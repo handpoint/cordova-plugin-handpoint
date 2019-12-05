f1d1f0292d3b44e7f5613c4fe79a4255921a64bf
Ezequiel Cuñado <ecunado@gmail.com>
2019-12-05 21:28:55 +0100

    Fixing error

--- src/android/com/handpoint/cordova/HandpointHelper.java
+++ src/android/com/handpoint/cordova/HandpointHelper.java
@@ -351,16 +351,16 @@ public class HandpointHelper implements Events.Required, Events.Status, Events.L
     this.callbackContext.sendPluginResult(result);
   }
 
-  protected Map<String, Object> getExtraParams(JSONObject params) throws JSONException {
+  protected Map<String, String> getExtraParams(JSONObject params) throws JSONException {
     if (params.has("map")) {
       return this.jsonToMap((JSONObject) params.get("map"));
     } else {
-      return new HashMap<String, Object>();
+      return new HashMap<String, String>();
     }
   }
 
-  protected static Map<String, Object> jsonToMap(JSONObject json) throws JSONException {
-    Map<String, Object> retMap = new HashMap<String, Object>();
+  protected static Map<String, String> jsonToMap(JSONObject json) throws JSONException {
+    Map<String, String> retMap = new HashMap<String, String>();
 
     if (json != JSONObject.NULL) {
       retMap = toMap(json);
@@ -368,40 +368,16 @@ public class HandpointHelper implements Events.Required, Events.Status, Events.L
     return retMap;
   }
 
-  protected static Map<String, Object> toMap(JSONObject object) throws JSONException {
-    Map<String, Object> map = new HashMap<String, Object>();
+  protected static Map<String, String> toMap(JSONObject object) throws JSONException {
+    Map<String, String> map = new HashMap<String, String>();
 
     Iterator<String> keysItr = object.keys();
     while (keysItr.hasNext()) {
       String key = keysItr.next();
-      Object value = object.get(key);
-
-      if (value instanceof JSONArray) {
-        value = toList((JSONArray) value);
-      }
-
-      else if (value instanceof JSONObject) {
-        value = toMap((JSONObject) value);
-      }
+      String value = object.get(key).toString();
       map.put(key, value);
     }
     return map;
-  }
-
-  protected static List<Object> toList(JSONArray array) throws JSONException {
-    List<Object> list = new ArrayList<Object>();
-    for (int i = 0; i < array.length(); i++) {
-      Object value = array.get(i);
-      if (value instanceof JSONArray) {
-        value = toList((JSONArray) value);
-      }
-
-      else if (value instanceof JSONObject) {
-        value = toMap((JSONObject) value);
-      }
-      list.add(value);
-    }
-    return list;
   }
 
   protected void finalize() {
