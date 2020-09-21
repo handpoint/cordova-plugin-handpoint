e1638834c4a690f525ef0569aa5c5ee21bb43352
Ezequiel Cuñado <ecunado@gmail.com>
2020-09-21 02:23:25 +0200

    Fixing TenderType & PaymentScenario Enums JSON mapping

---
+++ src/android/com/handpoint/cordova/GsonTenderTypeAdapter.java
@@ -0,0 +1,27 @@
+package com.handpoint.cordova;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.handpoint.api.shared.TenderType;
+
+import java.lang.reflect.Type;
+
+public class GsonTenderTypeAdapter implements JsonSerializer<TenderType>, JsonDeserializer<TenderType> {
+
+  @Override
+  public JsonElement serialize(TenderType tenderType, Type type, JsonSerializationContext context) {
+    return new JsonPrimitive(tenderType.toString());
+  }
+
+  @Override
+  public TenderType deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) throws JsonParseException {
+    return TenderType.Companion.fromString(jsonElement.getAsString());
+  }
+
+}
+
